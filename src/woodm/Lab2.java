b9dc93aeda9c908bc58f63037fc36ac0ba0c7fff
woodrmichael <woodm@msoe.edu>
2024-01-25 07:52:34 -0600

    JavaFX Application

---
+++ src/woodm/Lab2.java
@@ -0,0 +1,11 @@
+package woodm;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Lab2 extends Application {
+    @Override
+    public void start(Stage stage) throws Exception {
+
+    }
+}
