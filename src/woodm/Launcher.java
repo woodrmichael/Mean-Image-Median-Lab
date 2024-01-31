aebf8fd53175a9b9a31b813987040deef245cf22
woodrmichael <woodm@msoe.edu>
2024-01-30 21:24:05 -0600

    Lab Completed

--- src/woodm/Launcher.java
+++ src/woodm/Launcher.java
@@ -1,5 +1,15 @@
+/*
+ * Course: CSC1120A 121
+ * Spring 2023
+ * Lab 2 - Mean Image Median
+ * Name: Michael Wood
+ * Updated: 1/30/2024
+ */
 package woodm;
 
+/**
+ * Used to run the Jar file
+ */
 public class Launcher {
     public static void main(String[] args) {
         Lab2.main(args);
