8ba1e021e057be91aa3754cf5f3bd807992a9e05
woodrmichael <woodm@msoe.edu>
2024-01-31 23:47:06 -0600

    FXML shell

---
+++ src/woodm/Lab3.java
@@ -0,0 +1,28 @@
+/*
+ * Course: CSC1120A 121
+ * Spring 2023
+ * Lab 3 - Mean Image Median
+ * Name: Michael Wood
+ * Created: 1/31/2024
+ */
+package woodm;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+/**
+ * Creates an interactive GUI to calculate the mean or median on a set of images
+ */
+public class Lab3 extends Application {
+    @Override
+    public void start(Stage stage) throws IOException {
+        Parent pane = FXMLLoader.load(getClass().getResource("layout.fxml"));
+        stage.setScene(new Scene(pane));
+        stage.show();
+    }
+}
