25d4d5733bf8ffef7ab3771c1048812efc056a1b
woodrmichael <woodm@msoe.edu>
2024-02-01 09:29:05 -0600

    Input Images

--- src/woodm/Controller.java
+++ src/woodm/Controller.java
@@ -1,10 +1,19 @@
+/*
+ * Course: CSC1120A 121
+ * Spring 2023
+ * Lab 3 - Mean Image Median
+ * Name: Michael Wood
+ * Created: 2/1/2024
+ */
 package woodm;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.ScrollPane;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.layout.HBox;
 import javafx.stage.FileChooser;
 
 import java.io.IOException;
@@ -15,18 +24,32 @@ public class Controller {
     @FXML
     private Button addImageButton;
     @FXML
-    private ImageView imageView;
+    private ScrollPane scrollPane;
+    @FXML
+    private HBox imageContainer;
+    @FXML
+    private Button saveButton;
+    private Image finalImage;
     private ArrayList<Image> inputImages = new ArrayList<>();
-
+    private FileChooser fileChooser;
+    // Create a constructor to create FileChooser and also limit the extensions also
+    public Controller() {
+        this.fileChooser = new FileChooser();
+        fileChooser.getExtensionFilters().add(
+                new FileChooser.ExtensionFilter("Image Files", "*.png", "*.jpg", "*.ppm"));
+    }
     @FXML
     public void inputImage(ActionEvent actionEvent) throws IOException {
         //getExtensionFilters .addAll new ExtensionFilter // filter to .ppm, .png, .jpg
-        FileChooser fileChooser = new FileChooser();
-        Path imagePath = fileChooser.showOpenDialog(null).toPath();
+        Path imagePath = this.fileChooser.showOpenDialog(null).toPath();
         Image image = MeanImageMedian.readImage(imagePath);
         this.inputImages.add(image);
-        this.imageView.setX(image.getWidth());
-        this.imageView.setY(image.getHeight());
-        this.imageView.setImage(image);
+        this.imageContainer.getChildren().add(new ImageView(image));
+    }
+
+    @FXML
+    public void save(ActionEvent actionEvent) throws IOException {
+        Path imagePath = this.fileChooser.showSaveDialog(null).toPath();
+        MeanImageMedian.writeImage(imagePath, this.finalImage);
     }
 }
